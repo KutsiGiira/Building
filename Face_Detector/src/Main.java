d38d0793c386495b88bae36e0d21455d276429a9
KutsiGiira <haytamboualkhours@gmail.com>
2024-12-10 18:48:06 +0100

    Learning openCv

--- Face_Detector/src/Main.java
+++ Face_Detector/src/Main.java
@@ -1,5 +1,25 @@
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat p = Imgcodecs.imread("D:\\Apps\\Building\\Face_Detector\\src\\JAVA.webp");
+        if(p.empty()){
+            System.out.println("No pic founded");
+            return;
+        }
+            HighGui.imshow("Image", p);
+        HighGui.waitKey(1000);
+        System.out.println(p.cols());
+        System.out.println(p.channels());
+        Mat g = new Mat();
+        Imgproc.cvtColor(p, g, Imgproc.COLOR_BGR2GRAY);
+        HighGui.imshow("Image", g);
+        HighGui.waitKey(0);
     }
 }
