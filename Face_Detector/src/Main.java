570c78b5f2cf02b39dbdc4badf491744754e34b1
KutsiGiira <haytamboualkhours@gmail.com>
2024-12-17 19:08:01 +0100

    Testing without camera

--- Face_Detector/src/Main.java
+++ Face_Detector/src/Main.java
@@ -1,25 +1,35 @@
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.videoio.VideoCapture;
 
 public class Main {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat p = Imgcodecs.imread("D:\\Apps\\Building\\Face_Detector\\src\\JAVA.webp");
-        if(p.empty()){
-            System.out.println("No pic founded");
+        VideoCapture camera = new VideoCapture(0);
+        CascadeClassifier faceDetector = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        Mat f = new Mat();
+        if(!camera.isOpened()){
+            System.out.println("no Camera");
             return;
         }
-            HighGui.imshow("Image", p);
-        HighGui.waitKey(1000);
-        System.out.println(p.cols());
-        System.out.println(p.channels());
-        Mat g = new Mat();
-        Imgproc.cvtColor(p, g, Imgproc.COLOR_BGR2GRAY);
-        HighGui.imshow("Image", g);
+        while(true){
+            camera.read(f);
+            if(f.empty()){
+                break;
+            }
+            MatOfRect faceR = new MatOfRect();
+            faceDetector.detectMultiScale(f, faceR);
+        for(Rect rect : faceR.toArray()) {
+            Imgproc.rectangle(f, new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(0, 255, 0), 3);
+        }
+        HighGui.imshow("face", f);
         HighGui.waitKey(0);
+        }
+        camera.release();
     }
 }
